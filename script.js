f7b573740f7ef79fb5bf47b3770471a248fd14c3
stevebagz <drathok@gmail.com>
2024-03-18 21:49:21 -0300

    fix glow error on project page

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const nameText = document.getElementsByClassName("nav-glow");
+var nameText = document.getElementsByClassName("nav-glow");
 
-for(let i = 0; i < nameText.length; i++) {
-    nameText[i].addEventListener("mouseover", () => {
-        nameText[i].style.transition="all, 1.2s";
-    });
-}
+document.addEventListener("DOMContentLoaded", function() {
+    for(let i = 0; i < nameText.length; i++) {
+        nameText[i].addEventListener("mouseover", () => {
+            nameText[i].style.transition="all 1.2s";
+        });
+    }
+});
 
 
 
